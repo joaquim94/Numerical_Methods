7b3a3b7d8b16777bf8711be759589ca1f16a850a
joaquim94 <joaquimbrugues@hotmail.es>
2016-12-08 01:06:30 +0100

    Add files via upload

--- assignment10/assignment10A.m
+++ assignment10/assignment10A.m
@@ -4,12 +4,13 @@ clear all
 %mu x vx T (half period).
 
 default_dt = 3;
-refined_dt = 1;
-refined = [32,46,87,118,267,361,478,577,582,593:596,620:632,744,779,813,976,1020,1039,1054,1069:1075,1148,1220,1396,1399,1423:1480,1699,1819,1823,1827,2167];
+%refined_dt = 1;
+%refined = [32,46,87,118,267,361,478,577,582,593:596,620:632,744,779,813,976,1020,1039,1054,1069:1075,1148,1220,1396,1399,1423:1480,1699,1819,1823,1827,2167];
 s = 1e-4;
 mu = [0.001:1e-5:0.015, 0.015:1e-4:0.05, 0.05:1e-3:0.49];
 n = length(mu);
 g = [0 1 0 0];
+
 tol = input('Desired tolerance (default = 1e-8): ');
 if isempty(tol) tol = 1e-8; end
 begin_with = input('Select index to start debugging (default = 1): ');
@@ -20,11 +21,7 @@ for i=begin_with:n
     [v,L] = select_eigenvector(mu(i),3,1);
     x0 = L - s*v;
     F = RTBPfield(mu(i));
-    if any(i==refined)
-        [ycrossing,T] = poincare_section(F,g,tol,0,x0,refined_dt);
-    else
-        [ycrossing,T] = poincare_section(F,g,tol,0,x0,default_dt);
-    end
+    [ycrossing,T] = poincare_section(F,g,tol,0,x0,default_dt);
     fprintf(fileID,'%.8f %.8f %.8f %.8f\n',mu(i),ycrossing(1),ycrossing(3),T);
     fprintf('Iteration %d/%d.\n',i,n)
 end
--- assignment10/assignment10plot.m
+++ assignment10/assignment10plot.m
@@ -14,7 +14,7 @@ fclose(fileID);
 
 switch k
     case 1
-        plot(results(:,1),results(:,3),'.')
+        plot(results(:,1),results(:,3),'.','MarkerSize',1)
         title('Plot of mu-velocity in (0.001,0.5)')
         xlabel('mu'); ylabel('vx');
     case 2
@@ -22,11 +22,11 @@ switch k
         while (results(i,1)<0.1)
             i = i+1;
         end
-        plot(results(1:i,1),results(1:i,3),'.')
+        plot(results(1:i,1),results(1:i,3),'.','MarkerSize',1)
         title('Plot of mu-velocity in (0.001,0.1)')
         xlabel('mu'); ylabel('vx');
     case 3
-        plot(results(:,1),results(:,2),'.')
+        plot(results(:,1),results(:,2),'.','MarkerSize',1)
         title('Plot of mu-x in (0.001,0.5)')
         xlabel('mu'); ylabel('x');
 end
